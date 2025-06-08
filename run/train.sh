d421e016c2f5a7fc073e7ba782814913068986ac
WoojinChung <58346392+gartland98@users.noreply.github.com>
2025-06-09 01:13:56 +0900

    Update train.sh

--- run/train.sh
+++ run/train.sh
@@ -6,4 +6,4 @@ export HF_HUB_ENABLE_HF_TRANSFER=1
 ACCELERATE_LOG_LEVEL=info TRANSFORMERS_VERBOSITY=info TRAINING_TYPE=PRETRAIN accelerate launch -m \
     --config_file accelerate/ds3.yaml \
     scripts.run_alignment \
-    recipes/samples/14m.yaml
+    recipes/samples/196k.yaml   #recipes/samples/49k.yaml 
